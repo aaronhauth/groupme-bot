bdc398b000716831e4a0842968d2526627bc9cb6
Aaron Hauth <aaron.hauth@realtracs.com>
2018-08-02 11:03:23 -0500

    fdsa

--- bot.js
+++ bot.js
@@ -5,11 +5,34 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/roll$/;
+      d20 = /^\/d20$/,
+      d4 = /^\/d4$/,
+      d6 = /^\/d6$/,
+      d8 = /^\/d8$/,
+      d10 = /^\/d10$/,
+      d12 = /^\/d12$/;
 
-  if(request.text && botRegex.test(request.text)) {
+  if(request.text) {
     this.res.writeHead(200);
     rollDie(request, 20)
+    if (d20.test(request.text)){
+      rollDie(request, 20)
+    }
+    else if (d4.test(request.text)){
+      rollDie(request, 4)
+    }
+    else if (d6.test(request.text)){
+      rollDie(request, 6)
+    }
+    else if (d8.test(request.text)){
+      rollDie(request, 8)
+    }
+    else if (d10.test(request.text)){
+      rollDie(request, 10)
+    }
+    else if (d12.test(request.text)){
+      rollDie(request, 12)
+    }
     this.res.end();
   } else {
     console.log("don't care");
@@ -28,7 +51,7 @@ function rollDie(request, size){
 
   if (roll == 20 && size == 20)
     botResponse += ". A CRITICAL SUCCESS!"
-    
+
   postMessage(botResponse)
 }
 
